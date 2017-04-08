afdb6e473727ceeb58a0d0f382c9c558e3932740
vanessamuller <vane9090@gmail.com>
2017-04-08 10:48:56 -0400

    added boostrap right navbar template

--- day7/jquery.js
+++ day7/jquery.js
@@ -1,7 +1,3 @@
-$("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
 
 
     /*! jQuery v1.11.1 | (c) 2005, 2014 jQuery Foundation, Inc. | jquery.org/license */
